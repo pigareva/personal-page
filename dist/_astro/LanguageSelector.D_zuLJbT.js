c8eee370c129fffaafedf2808f19b69d9c34ecc5
Olga Pigareva <olga@pigareva.de>
2026-03-31 13:43:39 +0200

    add style

---
+++ dist/_astro/LanguageSelector.D_zuLJbT.js
@@ -0,0 +1,1 @@
+import{LanguageSelector as p}from "./LanguageSelector.CEP1EUFU.js";import"./createLucideIcon.iEvCykEP.js";import"./index.Ci4zACTm.js";import"./index.CPdA1Jx3.js";export{p as LanguageSelector};
